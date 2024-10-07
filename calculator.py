93fd823f2f13163be1f152e7678205764a750197
Kamau Augustion <kpa403@usask.ca>
2024-10-07 13:28:48 -0600

    I have implemented the addition operation.

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# calculator
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+user = input("Enter Operation (+,-,*,/): ")
+
+if user == "+":
+    result = num1 + num2
+    print("The sum is", result)
