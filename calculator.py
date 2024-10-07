4000220cf89d8642e54240d2d7f54cab2ed053d8
Kamau Augustion <kpa403@usask.ca>
2024-10-07 14:14:13 -0600

    I have implemented the division operation and also checks if the the second number entered is not 0. Otherwise return error message.

--- calculator.py
+++ calculator.py
@@ -2,17 +2,34 @@
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
 user = input("Enter Operation (+,-,*,/): ")
 
+#Calculating the operation.
+
+#Addition.
 if user == "+":
     result = num1 + num2
     print("The sum is", result)
 
+#Substraction.
 elif user == "-":
     result = num1 - num2
     print("The difference is", result)
 
+#Multiplication.
 elif user == "*":
     result = num1 * num2
     print("The product is", result)
+
+#Division
+elif user =="/":
+    #Check that the second number input is not divisible by 0.
+    if num2 != 0:
+        result = num1 / num2
+        print("The product is", result)
+
+    else:
+        print("Error: num1 is not divisible by 0")
+
+else:
+    print("Invalid entry.")
