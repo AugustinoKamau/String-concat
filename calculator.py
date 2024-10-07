1f47b4938ae8b68b8d0ecf125874afe8f2c5f209
Kamau Augustion <kpa403@usask.ca>
2024-10-07 14:03:05 -0600

    Refactored the input num1 and 2 to accept float input. Also added two more implemenattion of substract, multiplication.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,18 @@
 # calculator
 
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 user = input("Enter Operation (+,-,*,/): ")
 
 if user == "+":
     result = num1 + num2
     print("The sum is", result)
+
+elif user == "-":
+    result = num1 - num2
+    print("The difference is", result)
+
+elif user == "*":
+    result = num1 * num2
+    print("The product is", result)
